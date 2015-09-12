80d0cb746a88f7d942d38858aad6767159dadd4d
WebDevStu <git@stewart-anderson.co.uk>
2015-09-12 22:51:18 +0100

    Starting new style of site

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path =	"/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+output_style = :expanded
+line_comments = true;
+#sass_options = { :debug_info => true }
