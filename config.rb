e8581b9a4865bdbe84b41a5674c454baf425d31d
WebDevStu <git@stewart-anderson.co.uk>
2015-11-16 22:04:02 +0000

    Adding more projects

--- config.rb
+++ config.rb
@@ -3,6 +3,6 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
-output_style = :expanded
+output_style = :compressed
 line_comments = true;
 #sass_options = { :debug_info => true }
