32a0ac4aa886b8dab62cc2e49693f77f34d1b3ed
WebDevStu <git@stewart-anderson.co.uk>
2016-02-20 22:06:35 +0000

    Setting up deps manager for retrieving before app can start

--- js/app.js
+++ js/app.js
@@ -1,30 +1,52 @@
 
 ;(function () {
 
-    var config = {
+    var app,
+        config = {
             content: null,
             projects: null
         },
 
-        
+        responseManager = function (expected, callback) {
 
-        responseHandler = function (type) {
+            var called = 0;
+
+            return function () {
+
+                called += 1;
+
+                if (called === expected) {
+                    callback();
+                }
+            };
+        },
+
+        responseHandler = function (type, complete) {
             return function (response) {
-                config[type] = reponse;
+
+                config[type] = response;
+                complete();
             };
         },
 
         errorHandler = function (reponse) {
             // @TODO
             console.error('error', response);
-        };
+        },
+
+
+        startApp = function () {
+            console.log('start app');
+        },
+
+        registerComplete = responseManager(2, startApp);
 
 
     // fetch projects
-    fetch('/content/projects.json').then(responseHandler('projects'), errorHandler);
+    fetch('/content/projects.json').then(responseHandler('projects', registerComplete), errorHandler);
 
     // fetch content
-    fetch('/content/content.json').then(responseHandler('content'), errorHandler);
+    fetch('/content/content.json').then(responseHandler('content', registerComplete), errorHandler);
 
 
 
